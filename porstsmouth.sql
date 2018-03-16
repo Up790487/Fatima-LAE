2b52fbe966a90c3a27101f0594ccf50a8b7cd9b6
Fatima Alsayegh <up790487@myport.ac.uk>
2018-03-16 10:10:23 +0000

    Update porstsmouth.sql

--- porstsmouth.sql
+++ porstsmouth.sql
@@ -1,19 +1,7 @@
--- MySQL dump 10.13  Distrib 5.7.12, for osx10.9 (x86_64)
+
 --
 -- Host: 127.0.0.1    Database: Portsmouth
 -- ------------------------------------------------------
--- Server version	5.7.16
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
 -- Table structure for table `attraction`
@@ -133,14 +121,4 @@ LOCK TABLES `user` WRITE;
 INSERT INTO `user` VALUES (1,'tourist','tourist123','Tourist Firstname','Tourist Surname','0775899994','a.adeseye007@gmail.com','23 Southampton Road, Portsmouth Hampshire PO6 4SH',1),(2,'admin','admin123','Admin','Admin Lastname','08557773','aish.lapy@gmail.com','University House\nWinston Churchill Avenue\nPortsmouth\nHampshire\nPO1 2UP',1),(4,'tourist2','tourist2','Tourist 2','Tourist','786798715','adeyemi@brilloconnetz.com','23 Southampton Road, Portsmouth Hampshire PO6 4SH',1);
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2018-03-15 18:57:08
